92f37b420f35c8859a8cd4240693467a50daabb1
Jovan Popovic <popovicjovan23@gmail.com>
2021-04-25 22:27:17 +0200

    Updated composer.json

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -13,7 +13,7 @@
 <?php foreach ($data['posts'] as $post) : ?>
     <div class="card card-body mb-3">
         <h4 class="card-title"><?php echo $post->title; ?></h4>
-        <div class="bg-light p-2 mb-3">Written By:<?php echo $post->name; ?> on <?php echo $post->postCreated; ?></div>
+        <div class="bg-light p-2 mb-3">Written By: <?php echo $post->name; ?> on <?php echo $post->postCreated; ?></div>
         <p class="card-body"><?php echo $post->body; ?></p>
         <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
     </div>
