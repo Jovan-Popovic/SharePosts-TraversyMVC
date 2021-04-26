bd5782cbee3a6c2f3212aac31e38825b0f4403ae
Jovan Popovic <popovicjovan23@gmail.com>
2021-04-26 20:14:16 +0200

    Remote DB is set

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,22 @@
 <?php
 
-// DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '123456');
-define('DB_NAME', 'shareposts');
+// DB Params for Local DB
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '123456');
+// define('DB_NAME', 'shareposts');
+
+// DB Params for Remote DB
+define('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'qXdAxQNETF');
+define('DB_PASS', 'LLhgJi2EqR');
+define('DB_NAME', 'qXdAxQNETF');
 
 // App root
 define('APPROOT',  dirname(dirname(__FILE__)));
 // URL root
-define('URLROOT', 'http://localhost/shareposts');
+define('URLROOT', 'https://shareposts-traversymvc.herokuapp.com');
 // Site name
 define('SITENAME', 'SharePosts');
-
-// Site name
+// App version
 define('APPVERION', '1.0.0');
